eadbeb42775ced9ecd853663808caf8e3e84db70
ZsZs <zsolt.zsuffa@gmail.com>
2012-03-02 08:51:48 +0100

    Extract method: lookUpRepositoriesAndFactories()

--- Implementation/DomainTier/ObjectTests/com/processpuzzle/address/artifact/SettlementDataSheetFactoryTestFixture.java
+++ Implementation/DomainTier/ObjectTests/com/processpuzzle/address/artifact/SettlementDataSheetFactoryTestFixture.java
@@ -1,15 +1,27 @@
 package com.processpuzzle.address.artifact;
 
+import hu.itkodex.commons.persistence.UnitOfWork;
 import hu.itkodex.litest.template.ArtifactFactoryTestEnvironment;
 import hu.itkodex.litest.template.ArtifactFactoryTestFixture;
+import hu.itkodex.litest.template.DefaultApplicationFixture;
 
 import com.processpuzzle.address.artifact.SettlementDataSheetFactory;
 import com.processpuzzle.address.artifact.SettlementDataSheetRepository;
+import com.processpuzzle.address.domain.Country;
+import com.processpuzzle.address.domain.CountryFactory;
+import com.processpuzzle.address.domain.CountryRepository;
+import com.processpuzzle.address.domain.Settlement;
 import com.processpuzzle.address.domain.SettlementRepository;
+import com.processpuzzle.artifact_type.domain.ArtifactType;
+import com.processpuzzle.persistence.domain.DefaultUnitOfWork;
 
 public class SettlementDataSheetFactoryTestFixture extends ArtifactFactoryTestFixture<SettlementDataSheetFactory, SettlementDataSheet> {
    public static final String COUNTRY_NAME = "Magyarország";
+   private static final String SETTLEMENT_DATA_SHEET_TYPE_NAME = "SettlementDataSheet";
    private static final String SETTLEMENT_NAME = "Szada";
+   private Country country;
+   private CountryFactory countryFactory;
+   private CountryRepository countryRepository;
    private SettlementDataSheetRepository settlementDataSheetRepository;
    private SettlementRepository settlementRepository;
 
@@ -31,14 +43,35 @@ public class SettlementDataSheetFactoryTestFixture extends ArtifactFactoryTestFi
    @Override
    protected void configureBeforeSutInstantiation() {
       super.configureBeforeSutInstantiation();
-      settlementDataSheetRepository = applicationContext.getRepository( SettlementDataSheetRepository.class );
-      settlementRepository = applicationContext.getRepository( SettlementRepository.class );
+      lookUpRepositoriesAndFactories();
+      createArtifactTypeForSettlementDataSheet();
    }
 
-   @Override protected void configureAfterSutInstantiation() {}
+   @Override protected void configureAfterSutInstantiation() {
+      country = countryFactory.createCountry( COUNTRY_NAME );
+      UnitOfWork work = new DefaultUnitOfWork( true );
+      countryRepository.add( work, country );
+      work.finish();
+   }
 
    @Override protected void releaseResources() {
-      // TODO Automatikusan elõállított metóduscsonk
+      UnitOfWork work = new DefaultUnitOfWork( true );
+      countryRepository.delete( work, country );
+      work.finish();
+   }
+   
+   private void createArtifactTypeForSettlementDataSheet(){
+      ArtifactType settlementDataSheetType = artifactTypeFactory.createArtifactType( SETTLEMENT_DATA_SHEET_TYPE_NAME, DefaultApplicationFixture.SYSTEM_ADMINISTRATION_ARTIFACT_TYPE_GROUP, SettlementDataSheet.class );
+      settlementDataSheetType.setDomainClassName( Settlement.class.getName() );
+      UnitOfWork work = new DefaultUnitOfWork( true );
+      artifactTypeRepository.add( work, settlementDataSheetType );
+      work.finish();
    }
 
+   private void lookUpRepositoriesAndFactories() {
+      settlementDataSheetRepository = applicationContext.getRepository( SettlementDataSheetRepository.class );
+      settlementRepository = applicationContext.getRepository( SettlementRepository.class );
+      countryFactory = applicationContext.getEntityFactory( CountryFactory.class );
+      countryRepository = applicationContext.getRepository( CountryRepository.class );
+   }
 }
