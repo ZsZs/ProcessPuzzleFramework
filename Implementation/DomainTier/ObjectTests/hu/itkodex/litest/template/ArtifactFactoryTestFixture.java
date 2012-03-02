099a85f6776ec4f5660e79d76339dbd88dd90977
ZsZs <zsolt.zsuffa@gmail.com>
2012-03-02 08:53:25 +0100

    Implement lookUpArtifactTypeRepositoryAndFactory() method which instantiates artifactTypeFactory and artifactTypeRepository.

--- Implementation/DomainTier/ObjectTests/hu/itkodex/litest/template/ArtifactFactoryTestFixture.java
+++ Implementation/DomainTier/ObjectTests/hu/itkodex/litest/template/ArtifactFactoryTestFixture.java
@@ -5,9 +5,13 @@ import hu.itkodex.commons.generics.GenericTypeParameterInvestigator;
 import com.processpuzzle.application.configuration.domain.ProcessPuzzleContext;
 import com.processpuzzle.artifact.domain.Artifact;
 import com.processpuzzle.artifact.domain.ArtifactFactory;
+import com.processpuzzle.artifact_type.domain.ArtifactTypeFactory;
+import com.processpuzzle.artifact_type.domain.ArtifactTypeRepository;
 
 public abstract class ArtifactFactoryTestFixture<S extends ArtifactFactory<A>, A extends Artifact<?>> extends GenericTemplatedFixture<S> {
    private ConfigurableApplicationFixture applicationFixture;
+   protected ArtifactTypeFactory artifactTypeFactory;
+   protected ArtifactTypeRepository artifactTypeRepository;
    protected Class<A> artifactClass;
    protected ProcessPuzzleContext applicationContext;
    protected S factory;
@@ -17,16 +21,22 @@ public abstract class ArtifactFactoryTestFixture<S extends ArtifactFactory<A>, A
    protected void configureBeforeSutInstantiation() {
       artifactClass = (Class<A>) GenericTypeParameterInvestigator.getTypeParameter( this.getClass(), 1 );
       applicationContext = applicationFixture.getApplicationContext();
-      factory = (S) applicationContext.getEntityFactory( sutClass );
+      lookUpArtifactTypeRepositoryAndFactory();
    }
 
    @Override
    protected S instantiateSUT() {
+      factory = (S) applicationContext.getEntityFactory( sutClass );
       return factory;
    }
 
    protected ArtifactFactoryTestFixture( ArtifactFactoryTestEnvironment<S, ?> testEnvironment ) {
       super( testEnvironment );
       this.applicationFixture = testEnvironment.getApplicationFixture();
+   }
+   
+   private void lookUpArtifactTypeRepositoryAndFactory(){
+      artifactTypeFactory = applicationContext.getEntityFactory( ArtifactTypeFactory.class );
+      artifactTypeRepository = applicationContext.getRepository( ArtifactTypeRepository.class );
    }
 }
