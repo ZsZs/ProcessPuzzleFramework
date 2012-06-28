5b96766ec554be700297832284b924c04aa18e6a
ZsZs <zsolt.zsuffa@gmail.com>
2012-06-28 08:51:40 +0200

    Create Facebook connection.

---
+++ Implementation/WebTier/Source/com/processpuzzle/application/connection/control/CreateFacebookConnection.java
@@ -0,0 +1,7 @@
+package com.processpuzzle.application.connection.control;
+
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+
+public class CreateFacebookConnection {
+   FacebookConnectionFactory connectionFactory = new FacebookConnectionFactory( "1349240201", "clientSecret" );
+}
